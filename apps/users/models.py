9c3ba4c10fff8bd70bc02610f0a5f5286b837a83
emon51 <emanur11235@gmail.com>
2026-03-10 11:16:17 +0600

    fix(users): lowercase full email on normalization

--- apps/users/models.py
+++ apps/users/models.py
@@ -7,7 +7,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email: str, password: str, **extra_fields):
         if not email:
             raise ValueError("Email is required.")
-        email = self.normalize_email(email)
+        email = self.normalize_email(email).lower()  # lowercase full email
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
